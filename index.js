501f02e3e2119d92087904e981cdb8e44e9c98f9
sankalpaneupane <sneupane@hamropatro.com>
2024-08-08 10:29:51 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const io = require("socket.io")(8900, {
+    cors: {
+        origin: ["http://localhost:5173"],
+    }
+})
+
+//socket connection
+
+
+let users = [];
+const addUser = (userId, socketId)=> {
+    !users.some(user=> user.userId === userId) &&
+    users.push({userId, socketId})
+}
+
+const removeUser = (socketId)=> {
+    users = users.filter(user=> user.socketId !== socketId)
+}
+io.on("connection", (socket)=> {
+    socket.on("addUser", (userId)=> {
+        addUser(userId, socket.id)
+        io.emit("getUsers", users)
+    })
+    socket.on("sendMessage", ({senderId, receiverId, text})=> {
+        console.log(users)
+        console.log("senderId", senderId)
+        console.log("receiverId", receiverId)
+
+        console.log("text", text)
+        const user = users?.find(user=> user?.userId === receiverId)
+        if(user){
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text
+            })
+        }
+    })
+    socket.on("disconnect", ()=> {
+        removeUser(socket.id)
+        console.log("a user disconnected");
+    })
+})
